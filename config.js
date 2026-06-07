a5152ca9122351ffa9c6741ca76e5776b3cd33c4
Sami Kabbaj <kabbajsami2006@gmail.com>
2026-06-07 19:27:44 +0200

    chore: update title config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const QUIZ_CONFIG = {
-  title: "CS-202",
+  title: "CS-202: Quizzes",
   subtitle: "Computer Systems",
   engineCdn: "https://cdn.jsdelivr.net/gh/sami-epfl/ba4-quiz-engine@main",
   engineLocal: "../ba4-quiz-engine",
